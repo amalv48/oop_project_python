34b41f09900d697fc363f5c410e6f74b4ed06d3a
Adrian M <a.malvianto@yahoo.com>
2022-03-01 20:57:13 +1100

    fixed log path

--- jobs/sales.py
+++ jobs/sales.py
@@ -1,4 +1,4 @@
-from cmath import log
+#from cmath import log
 import logging
 import json, os, re, sys
 from typing import Callable, Optional
@@ -7,7 +7,13 @@ from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
 
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
-LOG_FILE = f"{project_dir}\logs\job-{os.path.basename(__file__)}.log"
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_FILE = f"{project_dir}/logs/job-{os.path.basename(__file__)}.log"
+#print(project_dir)
 LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
 logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format=LOG_FORMAT)
+logger = logging.getLogger('py4j')
+
+#print(sys.path)
+
+from classes import class_pyspark
